3738475a6c717d38f1b787427566b36eb1f2e9b8
gustavofelicidade <gustavosnik95@gmail.com>
2024-11-09 15:13:41 -0300

    Adding Class Main

--- interface/app.py
+++ interface/app.py
@@ -3,17 +3,20 @@ import pandas as pd
 from models.predict import predict_critical_cases
 from utils.helpers import load_model
 # from models import p
-st.title("Triagem de Pacientes - MJV Saúde ")
 
-uploaded_file = st.file_uploader("Escolha um arquivo CSV com dados de pacientes para triagem", type="csv")
+class Main:
+    def __init__(self):
+        st.title("Triagem de Pacientes - MJV Saúde ")
 
-if uploaded_file:
-    data = pd.read_csv(uploaded_file)
-    st.write("Dados Carregados:")
-    st.write(data.head())
+        uploaded_file = st.file_uploader("Escolha um arquivo CSV com dados de pacientes para triagem", type="csv")
 
-    model = load_model("data/model/trained_model.pkl")
-    # predictions = predict_critical_cases(model, data)
+        if uploaded_file:
+            data = pd.read_csv(uploaded_file)
+            st.write("Dados Carregados:")
+            st.write(data.head())
 
-    st.write("Previsões:")
-    # st.write(predictions)
+            model = load_model("data/model/trained_model.pkl")
+            # predictions = predict_critical_cases(model, data)
+
+            st.write("Previsões:")
+            # st.write(predictions)
--- main.py
+++ main.py
@@ -1,2 +1,17 @@
-import os
-os.system("streamlit run interface/app.py")
+import streamlit as st
+
+from interface.app import Main
+
+st.set_page_config(layout="wide",
+                   initial_sidebar_state="expanded",
+                   page_icon="👁️"
+                   )
+
+# ================================
+# START THE MJV Triage Nurse Assistant Application
+# ================================
+
+
+if __name__ == "__main__":
+    main_app = Main()
+    main_app.main()
